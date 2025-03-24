dfdceaa397f426c0a65109012fefca2f4eff5408
domuk-k <dannyworks102@gmail.com>
2025-03-24 19:07:27 +0900

    test: mock repository 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     implementation(libs.spring.boot.starter.web)
     annotationProcessor(libs.spring.boot.configuration.processor)
     testImplementation(libs.spring.boot.starter.test)
+   testImplementation("io.mockk:mockk:1.12.0")
 }
 
 // about source and compilation
--- src/test/kotlin/io/hhplus/tdd/point/PointServiceTest.kt
+++ src/test/kotlin/io/hhplus/tdd/point/PointServiceTest.kt
@@ -1,16 +1,17 @@
 package io.hhplus.tdd.point
 
-import io.hhplus.tdd.database.PointHistoryTable
-import io.hhplus.tdd.database.UserPointTable
 import io.hhplus.tdd.point.policy.PointPolicyImpl
 import io.hhplus.tdd.point.repository.PointHistoryRepository
-import io.hhplus.tdd.point.repository.PointHistoryRepositoryIml
-import io.hhplus.tdd.point.repository.PointRepositoryImpl
+import io.hhplus.tdd.point.repository.PointRepository
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.assertThrows
+
 
 class PointServiceTest {
 
@@ -18,65 +19,110 @@ class PointServiceTest {
     private val id = 1L
     private val MAX_BALANCE = 1234560L
 
+    private lateinit var pointHistoryRepository: PointHistoryRepository
+    private lateinit var pointRepository: PointRepository
+
     @BeforeEach
     fun setUp() {
-        val pointRepository = PointRepositoryImpl(UserPointTable())
-        val pointHistoryRepository = PointHistoryRepositoryIml(PointHistoryTable())
+        pointRepository = mockk<PointRepository>()
+        pointHistoryRepository = mockk<PointHistoryRepository>()
+
+        val now = System.currentTimeMillis()
+        every { pointRepository.save(any(), any()) } answers {
+            UserPoint(firstArg(), secondArg(), now)
+        }
+        every { pointHistoryRepository.save(any(), any(), any()) } answers {
+            PointHistory(1, firstArg(), thirdArg(), secondArg(), now)
+        }
+        every { pointRepository.findByUserId(id) } answers {
+            UserPoint(firstArg(), 1000L, now)
+        }
+
         val pointPolicy = PointPolicyImpl(MAX_BALANCE)
         pointService = PointService(pointRepository, pointHistoryRepository, pointPolicy)
     }
 
     @Test
-    fun `포인트 조회`() {
-        pointService.charge(id, 1000L)
+    fun `임의 유저의 포인트 조회할 수 있다`() {
         val point = pointService.point(id)
         assertEquals(1000L, point.point)
     }
 
     @Test
-    fun `포인트 충전 및 사용 내역 조회`() {
+    fun `포인트 충전,사용 후 포인트 변동 내역을 조회할 수 있다`() {
+        val now = System.currentTimeMillis()
+        every { pointHistoryRepository.findAllByUserId(id) } returns listOf(
+            PointHistory(1, id, TransactionType.CHARGE, 1000L, now),
+            PointHistory(2, id, TransactionType.USE, 200L, now)
+        )
+        every { pointRepository.save(any(), any()) } answers { UserPoint(id, 800L, now) }
+        every { pointRepository.findByUserId(any()) } answers { UserPoint(id, 800L, now) }
+
         pointService.charge(id, 1000L)
+
         val remainingPoint = pointService.use(id, 200L)
         val histories = pointService.history(id)
         assertEquals(2, histories.size)
         assertTrue(histories.any { it.type.name == "CHARGE" && it.amount == 1000L })
         assertTrue(histories.any { it.type.name == "USE" && it.amount == 200L })
+        println(remainingPoint)
         assertEquals(pointService.point(id), remainingPoint)
     }
 
-    @Test
-    fun `포인트 충전`() {
-        val updated = pointService.charge(id, 1500L)
-        assertEquals(1500L, updated.point)
+
+    @Nested
+    inner class 포인트충전 {
+        @Test
+        fun `충천 후 결과를 반환한다`() {
+            val updated = pointService.charge(id, 1000L)
+            assertEquals(1000L, updated.point)
+        }
+
+        @Test
+        fun `정책에서 정한 최대 잔고 이상으로 충전을 시도하면 예외가 발생한다`() {
+            assertThrows<IllegalArgumentException> {
+                pointService.charge(id, MAX_BALANCE + 1L)
+            }
+        }
+
     }
 
-    @Test
-    fun `포인트 사용`() {
-        pointService.charge(id, 2000L)
-        val updated = pointService.use(id, 500L)
-        assertEquals(1500L, updated.point)
-    }
+    @Nested
+    inner class 포인트_사용 {
+        @Test
+        fun `포인트 사용 후 결과를 반환한다`() {
+            val now = System.currentTimeMillis()
+            every { pointRepository.save(any(), any()) } answers { UserPoint(id, 1500L, now) }
 
-    @Test
-    fun `정책에서 정한 최대 잔고 이상으로 충전을 시도하면 예외 발생`() {
-        assertThrows<IllegalArgumentException> {
-            pointService.charge(id, MAX_BALANCE + 1L)
+
+            pointService.charge(id, 2000L)
+            val updated = pointService.use(id, 500L)
+            assertEquals(1500L, updated.point)
+        }
+
+        @Test
+        fun `충전 없이 포인트 사용 시 예외가 발생한다`() {
+            val deposit = 0L
+            val now = System.currentTimeMillis()
+            every { pointRepository.findByUserId(id) } returns UserPoint(id, deposit, now)
+
+            assertThrows<IllegalArgumentException> {
+                pointService.use(id, 500L)
+            }
+        }
+
+
+        @Test
+        fun `보유 포인트 초과 사용 시 예외가 발생한다`() {
+            val deposit = 300L
+            val now = System.currentTimeMillis()
+            every { pointRepository.findByUserId(id) } returns UserPoint(id, deposit, now)
+
+            pointService.charge(id, deposit)
+            assertThrows<IllegalArgumentException> {
+                pointService.use(id, deposit + 1L)
+            }
         }
     }
 
-    @Test
-    fun `충전 없이 포인트 사용 시 예외 발생`() {
-        assertThrows<IllegalArgumentException> {
-            pointService.use(id, 500L)
-        }
-    }
-
-
-    @Test
-    fun `보유 포인트 초과 사용 시 예외 발생`() {
-        pointService.charge(id, 300L)
-        assertThrows<IllegalArgumentException> {
-            pointService.use(id, 500L)
-        }
-    }
 }
