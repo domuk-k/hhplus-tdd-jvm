c69cb3d821727043487bef0d98db4479f63ca5d4
domuk-k <dannyworks102@gmail.com>
2025-03-28 00:24:31 +0900

    test: Controller 통합테스트 정정

--- src/test/kotlin/io/hhplus/tdd/point/PointControllerIntegrationTest.kt
+++ src/test/kotlin/io/hhplus/tdd/point/PointControllerIntegrationTest.kt
@@ -20,9 +20,7 @@ class PointControllerIntegrationTest(
 ) {
     @Test
     fun `포인트 조회 엔드포인트`() {
-        val userId = 1L
-
-        mockMvc.get("/point/$userId")
+        mockMvc.get("/point/1")
             .andExpect {
                 status { isOk() }
                 jsonPath("$.data.id") { value(1L) }
@@ -32,7 +30,7 @@ class PointControllerIntegrationTest(
 
     @Test
     fun `포인트 충전 엔드포인트`() {
-        val request = PointRequest.Charge(amount = 50)
+        val request = PointRequest.Charge(amount = 150)
         val requestJson = objectMapper.writeValueAsString(request)
 
         mockMvc.patch("/point/1/charge") {
@@ -41,44 +39,64 @@ class PointControllerIntegrationTest(
         }
             .andExpect {
                 status { isOk() }
-                jsonPath("$.id") { value(1) }
-                jsonPath("$.username") { value("testUser") }
-                // 100 + 50 = 150
-                jsonPath("$.points") { value(150) }
+                jsonPath("$.data.id") { value(1) }
+                jsonPath("$.data.point") { value(150) }
             }
     }
 
     @Test
-    fun `GET histories returns list of PointHistory`() {
-        mockMvc.get("/point/1/histories")
-            .andExpect {
-                status { isOk() }
-                jsonPath("$[0].id") { value(1) }
-                jsonPath("$[0].type") { value("charge") }
-                jsonPath("$[0].amount") { value(50) }
-                jsonPath("$[1].id") { value(1) }
-                jsonPath("$[1].type") { value("use") }
-                jsonPath("$[1].amount") { value(30) }
-            }
-    }
+    fun `포인트 사용 엔드포인트`() {
+        val toCharge = 50L
+        val toUse = 30L
+        val chargeRequest = PointRequest.Charge(amount = toCharge)
+        val chargeRequestJson = objectMapper.writeValueAsString(chargeRequest)
 
-    @Test
-    fun `PATCH use updates UserPoint`() {
-        val request = PointRequest.Use(amount = 30)
+        mockMvc.patch("/point/3/charge") {
+            contentType = MediaType.APPLICATION_JSON
+            content = chargeRequestJson
+        }
+
+        val request = PointRequest.Use(amount = toUse)
         val requestJson = objectMapper.writeValueAsString(request)
 
-        mockMvc.patch("/point/1/use") {
+        mockMvc.patch("/point/3/use") {
             contentType = MediaType.APPLICATION_JSON
             content = requestJson
         }
             .andExpect {
                 status { isOk() }
-                jsonPath("$.id") { value(1) }
-                jsonPath("$.username") { value("testUser") }
-                // 100 - 30 = 70
-                jsonPath("$.points") { value(70) }
+                jsonPath("$.data.id") { value(3) }
+                jsonPath("$.data.point") { value(toCharge - toUse) }
             }
     }
 
+    @Test
+    fun `포인트 변경내역 목록조회 엔드포인트`() {
 
+        val chargeRequest = PointRequest.Charge(amount = 50)
+        val chargeRequestJson = objectMapper.writeValueAsString(chargeRequest)
+
+        mockMvc.patch("/point/4/charge") {
+            contentType = MediaType.APPLICATION_JSON
+            content = chargeRequestJson
+        }
+
+        val useRequest = PointRequest.Charge(amount = 30)
+        val useRequestJson = objectMapper.writeValueAsString(useRequest)
+
+        mockMvc.patch("/point/4/use") {
+            contentType = MediaType.APPLICATION_JSON
+            content = useRequestJson
+        }
+        mockMvc.get("/point/4/histories")
+            .andExpect {
+                status { isOk() }
+                jsonPath("$.data.[0].userId") { value(4) }
+                jsonPath("$.data.[0].type") { value("CHARGE") }
+                jsonPath("$.data.[0].amount") { value(50) }
+                jsonPath("$.data.[1].userId") { value(4) }
+                jsonPath("$.data.[1].type") { value("USE") }
+                jsonPath("$.data.[1].amount") { value(30) }
+            }
+    }
 }
